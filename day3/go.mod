34f625584ae5c495313a3ce0f0b332ce5e25b23e
kabiru <mwangikabiru21@gmail.com>
2022-12-03 18:49:38 +0300

    Day 3 down. Someone somewhere did the whole alphabets

--- day3/go.mod
+++ day3/go.mod
@@ -1,3 +1,5 @@
 module day1
 
 go 1.18
+
+require github.com/go-deeper/chunks v1.0.0 // indirect
--- day3/main.go
+++ day3/main.go
@@ -5,6 +5,10 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
+	"unicode/utf8"
+
+	"github.com/go-deeper/chunks"
 )
 
 func readFile(filename string) []string {
@@ -31,8 +35,76 @@ func readFile(filename string) []string {
 	return data
 }
 
-func main() {
-	data := readFile("input.test.txt")
+func getValueOfLetter(letter string) int {
+	value, _ := utf8.DecodeRuneInString(letter)
+	result := 0
 
-	fmt.Println(data)
+	if strings.ToUpper(letter) == letter {
+		result = int(value) - 38
+		return result
+	}
+
+	if strings.ToLower(letter) == letter {
+		result = int(value) - 96
+		return result
+	}
+
+	return int(value)
+}
+
+func getCommonLetterInString(runsack string) string {
+	first := runsack[:len(runsack)/2]
+	second := runsack[len(runsack)/2:]
+
+	for l := range first {
+		if strings.Contains(second, string(first[l])) {
+			return string(first[l])
+		}
+	}
+
+	return ""
+}
+
+func getCommonLetterInArray(runsacks []string) string {
+	first := runsacks[0]
+	second := runsacks[1]
+	third := runsacks[2]
+
+	for l := range first {
+		if strings.Contains(second, string(first[l])) && strings.Contains(third, string(first[l])) {
+			return string(first[l])
+		}
+	}
+
+	return ""
+}
+
+func partOne(data []string) int {
+	total := 0
+
+	for d := range data {
+		commonLetter := getCommonLetterInString(data[d])
+		total += getValueOfLetter(commonLetter)
+	}
+
+	return total
+}
+
+func partTwo(data []string) int {
+	total := 0
+	chunk := chunks.Split(data, 3)
+
+	for c := range chunk {
+		commonLetter := getCommonLetterInArray(chunk[c])
+		total += getValueOfLetter(commonLetter)
+	}
+
+	return total
+}
+
+func main() {
+	data := readFile("input.txt")
+
+	fmt.Println(partOne(data))
+	fmt.Println(partTwo(data))
 }
