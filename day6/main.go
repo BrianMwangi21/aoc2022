82950ab331ff092b53ca2f3825c2ee93d9b9b299
kabiru <mwangikabiru21@gmail.com>
2022-12-06 09:36:26 +0300

    Day 6 was way way better than Day 5. Phew.

--- day6/main.go
+++ day6/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func readFile(filename string) []string {
@@ -31,8 +32,37 @@ func readFile(filename string) []string {
 	return data
 }
 
-func main() {
-	data := readFile("input.test.txt")
+func isRepeated(data string) bool {
+	for _, d := range data {
+		if strings.Count(data, string(d)) > 1 {
+			return true
+		}
+	}
+	return false
+}
 
-	fmt.Println(data)
+func partOneOrTwo(data string, WINDOW_SIZE int) int {
+	first_marker := 0
+
+	for d := range data[:len(data)-WINDOW_SIZE-1] {
+		window := ""
+
+		for i := 0; i < WINDOW_SIZE; i++ {
+			window += string(data[d+i])
+		}
+
+		if !isRepeated(window) {
+			first_marker = d + WINDOW_SIZE
+			break
+		}
+	}
+
+	return first_marker
+}
+
+func main() {
+	data := readFile("input.txt")
+
+	fmt.Println(partOneOrTwo(data[0], 4))
+	fmt.Println(partOneOrTwo(data[0], 14))
 }
