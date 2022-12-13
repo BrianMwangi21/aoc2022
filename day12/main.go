73f9d0e98fde7cea83e204ee0fc1f6752df915b6
kabiru <mwangikabiru21@gmail.com>
2022-12-14 00:36:21 +0300

    Day 12 commence

--- day12/main.go
+++ day12/main.go
@@ -7,6 +7,29 @@ import (
 	"os"
 )
 
+// https://www.youtube.com/watch?v=xcIUM003HS0
+
+type Coords struct {
+	row int
+	col int
+}
+
+func (c Coords) GetNeighbours() (Coords, Coords, Coords, Coords) {
+	up := Coords{c.row - 1, c.col}
+	right := Coords{c.row, c.col + 1}
+	down := Coords{c.row + 1, c.col}
+	left := Coords{c.row, c.col - 1}
+	return up, right, down, left
+}
+
+func isCoordContained(landscape [][]int) bool {
+	return false
+}
+
+func isMoveValid(from Coords, to Coords) bool {
+	return false
+}
+
 func readFile(filename string) []string {
 	var data []string
 
@@ -31,8 +54,44 @@ func readFile(filename string) []string {
 	return data
 }
 
+func parseData(data []string) ([][]int, Coords, Coords) {
+	var (
+		landscape  [][]int
+		startPoint Coords
+		endPoint   Coords
+	)
+
+	for row, d := range data {
+		var m []int
+		for col, l := range d {
+			m = append(m, int(l))
+			if string(l) == "S" {
+				startPoint.row = row
+				startPoint.col = col
+			} else if string(l) == "E" {
+				endPoint.row = row
+				endPoint.col = col
+			}
+		}
+		landscape = append(landscape, m)
+	}
+
+	return landscape, startPoint, endPoint
+}
+
+func partOne(landscape [][]int, startPoint Coords, endPoint Coords) int {
+	leastSteps := 0
+
+	for _, d := range landscape {
+		fmt.Println(d)
+	}
+
+	return leastSteps
+}
+
 func main() {
 	data := readFile("input.test.txt")
 
-	fmt.Println(data)
+	landscape, startPoint, endPoint := parseData(data)
+	fmt.Println(partOne(landscape, startPoint, endPoint))
 }
